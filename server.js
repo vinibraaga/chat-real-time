f6588b12de68b44ad0cdc6444b8428e231a47024
vinibraaga <vinibraaga@gmail.com>
2020-09-06 00:48:53 -0300

    Função previousMessages para não perder as menssagens ao recarregar a pag

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ let messages = [];
 io.on('connection', socket => {
     console.log(`Socket conectado: ${socket.id}`);
 
+    socket.emit('previousMessages', messages);
+
     socket.on('sendMessage', data => {
         messages.push(data);
         socket.broadcast.emit('receivedMessage',data)
